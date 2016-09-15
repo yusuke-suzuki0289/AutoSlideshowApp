6374540334b5fbaeed240811d88973f17e8edeee
yusuke-suzuki0289 <yusu.suz.0320@gmail.com>
2016-09-16 01:08:32 +0900

    modify

--- app/src/main/java/jp/techacademy/yusuke2/suzuki/autoslideshowapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/yusuke2/suzuki/autoslideshowapp/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -18,10 +19,10 @@ import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
-//    implements OnClickListener
 
     private static final int PERMISSIONS_REQUEST_CODE = 100;
-    int judge = 1; //判定用変数
+
+    int judge = 0; //判定用変数
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -70,64 +71,9 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     //初期画像表示
     public void getContentsInfo() {
 
-        // 画像の情報URIを全件取得する
-        ContentResolver resolver = getContentResolver();//ContentResolverでContentProviderのデータを参照
-        Cursor cursor = resolver.query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
-                null, // 項目(null = 全項目)
-                null, // フィルタ条件(null = フィルタなし)
-                null, // フィルタ用パラメータ
-                null // ソート (null ソートなし)
-        );
-
-        // indexからIDを取得し、そのIDから画像のURIを取得する
-        // URIとは目的のデータを示すために使われるもの。PCであればファイルをディレクトリで指定するが、AndroidではURIで指定する感じ。
-
-        if (judge == 1){
-            cursor.moveToFirst();
-            //URIが入っているカラムの箇所の取得
-            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-            //画像ファイルの固有番号を確認
-            Long id = cursor.getLong(fieldIndex);
-            //固有番号からURLの取得
-            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-
-            ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
-            imageVIew.setImageURI(imageUri);
-        }else if(judge == 2){
-            cursor.moveToNext();
-            //URIが入っているカラムの箇所の取得
-            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-            //画像ファイルの固有番号を確認
-            Long id = cursor.getLong(fieldIndex);
-            //固有番号からURLの取得
-            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-
-            ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
-            imageVIew.setImageURI(imageUri);
-        }else if(judge == 3) {
-            cursor.moveToPrevious();
-            //URIが入っているカラムの箇所の取得
-            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-            //画像ファイルの固有番号を確認
-            Long id = cursor.getLong(fieldIndex);
-            //固有番号からURLの取得
-            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-
-            ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
-            imageVIew.setImageURI(imageUri);
-//        }else if(judge == 4){
-//            cursor.moveToFirst();
-
-        }
-
-    }
-
-    public void onClick(View v) {
-
         // ボタンの取得とリスナーの登録
-        Button button = (Button) findViewById(R.id.button1);
-        button.setOnClickListener(this);
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
 
         Button button2 = (Button) findViewById(R.id.button2);
         button2.setOnClickListener(this);
@@ -138,22 +84,67 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         Button button4 = (Button) findViewById(R.id.button4);
         button4.setOnClickListener(this);
 
+        // indexからIDを取得し、そのIDから画像のURIを取得する
+        // URIとは目的のデータを示すために使われるもの。PCであればファイルをディレクトリで指定するが、AndroidではURIで指定する感じ。
+        // 画像の情報URIを全件取得する
+        ContentResolver resolver = getContentResolver();//ContentResolverでContentProviderのデータを参照
+        Cursor cursor = resolver.query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+                null, // 項目(null = 全項目)
+                null, // フィルタ条件(null = フィルタなし)
+                null, // フィルタ用パラメータ
+                null // ソート (null ソートなし)
+        );
+
+        if (judge == 0) {
+            cursor.moveToFirst();
+
+        }else if (judge == 1) {
+            cursor.moveToFirst();
+
+        } else if (judge == 2) {
+            cursor.moveToNext();
+
+        } else if (judge == 3) {
+            cursor.moveToPrevious();
+
+        }else if(judge == 4) {
+
+        }
+
+            //URIが入っているカラムの箇所の取得
+        int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+        //画像ファイルの固有番号を確認
+        Long id = cursor.getLong(fieldIndex);
+        //固有番号からURLの取得
+        Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+        ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
+        imageVIew.setImageURI(imageUri);
+    }
+
+    public void onClick(View v) {
+
         //先頭に戻るボタンが押された場合/
         if (v.getId() == R.id.button1) {
             judge = 1;
             getContentsInfo();
+            Log.d("javatest","ボタン１");
             //進むボタンが押された場合
         } else if (v.getId() == R.id.button2) {
             judge = 2;
             getContentsInfo();
+            Log.d("javatest","ボタン２");
             //戻るボタンが押された場合
         } else if (v.getId() == R.id.button3) {
             judge = 3;
             getContentsInfo();
+            Log.d("javatest","ボタン３");
             //再生ボタンが押された場合
         } else if (v.getId() == R.id.button4) {
             judge = 4;
             getContentsInfo();
+            Log.d("javatest","ボタン４");
         }
 
     }
