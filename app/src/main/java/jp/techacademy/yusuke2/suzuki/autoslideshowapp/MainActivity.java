74368e91c2d0a144ea23750322ce6130a87b53c5
yusuke-suzuki0289 <yusu.suz.0320@gmail.com>
2016-09-15 18:47:21 +0900

    修正

--- app/src/main/java/jp/techacademy/yusuke2/suzuki/autoslideshowapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/yusuke2/suzuki/autoslideshowapp/MainActivity.java
@@ -10,18 +10,30 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageView;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+//    implements OnClickListener
 
     private static final int PERMISSIONS_REQUEST_CODE = 100;
+    int judge = 1; //判定用変数
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    //パーミッションを表示する（Android 6.0以降の場合）
+        //パーミッションを表示する（Android 6.0以降の場合）
         // Android 6.0以降の場合
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             // パーミッションの許可状態を確認する
@@ -36,6 +48,9 @@ public class MainActivity extends AppCompatActivity {
         } else {
             getContentsInfo();
         }
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     //パーミッションに対するユーザの選択結果を受け取る
@@ -52,7 +67,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void getContentsInfo() {
+    //初期画像表示
+    public void getContentsInfo() {
 
         // 画像の情報URIを全件取得する
         ContentResolver resolver = getContentResolver();//ContentResolverでContentProviderのデータを参照
@@ -64,20 +80,82 @@ public class MainActivity extends AppCompatActivity {
                 null // ソート (null ソートなし)
         );
 
-        if (cursor.moveToFirst()) {
-            do {
-                // indexからIDを取得し、そのIDから画像のURIを取得する
-                // URIとは目的のデータを示すために使われるもの。PCであればファイルをディレクトリで指定するが、AndoroidではURIで指定する感じ。
-                int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-                Long id = cursor.getLong(fieldIndex);
-                Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+        // indexからIDを取得し、そのIDから画像のURIを取得する
+        // URIとは目的のデータを示すために使われるもの。PCであればファイルをディレクトリで指定するが、AndroidではURIで指定する感じ。
 
-                ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
-                imageVIew.setImageURI(imageUri);
+        if (judge == 1){
+            cursor.moveToFirst();
+            //URIが入っているカラムの箇所の取得
+            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+            //画像ファイルの固有番号を確認
+            Long id = cursor.getLong(fieldIndex);
+            //固有番号からURLの取得
+            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
 
-            } while (cursor.moveToNext());
+            ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
+            imageVIew.setImageURI(imageUri);
+        }else if(judge == 2){
+            cursor.moveToNext();
+            //URIが入っているカラムの箇所の取得
+            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+            //画像ファイルの固有番号を確認
+            Long id = cursor.getLong(fieldIndex);
+            //固有番号からURLの取得
+            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+            ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
+            imageVIew.setImageURI(imageUri);
+        }else if(judge == 3) {
+            cursor.moveToPrevious();
+            //URIが入っているカラムの箇所の取得
+            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+            //画像ファイルの固有番号を確認
+            Long id = cursor.getLong(fieldIndex);
+            //固有番号からURLの取得
+            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+            ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
+            imageVIew.setImageURI(imageUri);
+//        }else if(judge == 4){
+//            cursor.moveToFirst();
+
         }
-        cursor.close();
+
+    }
+
+    public void onClick(View v) {
+
+        // ボタンの取得とリスナーの登録
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(this);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+
+        //先頭に戻るボタンが押された場合/
+        if (v.getId() == R.id.button1) {
+            judge = 1;
+            getContentsInfo();
+            //進むボタンが押された場合
+        } else if (v.getId() == R.id.button2) {
+            judge = 2;
+            getContentsInfo();
+            //戻るボタンが押された場合
+        } else if (v.getId() == R.id.button3) {
+            judge = 3;
+            getContentsInfo();
+            //再生ボタンが押された場合
+        } else if (v.getId() == R.id.button4) {
+            judge = 4;
+            getContentsInfo();
+        }
+
     }
 
 }
