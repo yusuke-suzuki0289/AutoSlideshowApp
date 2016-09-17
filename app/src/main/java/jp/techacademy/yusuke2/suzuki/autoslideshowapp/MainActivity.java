39e1c4d1ebac4df4f0ced6ba97be8dd1e5f4744a
yusuke-suzuki0289 <yusu.suz.0320@gmail.com>
2016-09-17 17:47:51 +0900

    修正

--- app/src/main/java/jp/techacademy/yusuke2/suzuki/autoslideshowapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/yusuke2/suzuki/autoslideshowapp/MainActivity.java
@@ -17,15 +17,16 @@ import android.widget.Button;
 import android.widget.ImageView;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
-
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     private static final int PERMISSIONS_REQUEST_CODE = 100;
     int judge = 0; //判定用変数
     private Cursor cursor; //メンバ変数として宣言
-    private OnClick v;
+
+
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -101,58 +102,51 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
             );
 
             cursor.moveToFirst();
-            //URIが入っているカラムの箇所の取得
-            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-            //画像ファイルの固有番号を確認
-            Long id = cursor.getLong(fieldIndex);
-            //固有番号からURLの取得
-            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+            setImageURI();
 
-            ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
-            imageVIew.setImageURI(imageUri);
-
-        }else if (judge == 1) {
+        } else if (judge == 1) {
             cursor.moveToFirst();
-            //URIが入っているカラムの箇所の取得
-            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-            //画像ファイルの固有番号を確認
-            Long id = cursor.getLong(fieldIndex);
-            //固有番号からURLの取得
-            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-
-            ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
-            imageVIew.setImageURI(imageUri);
+            setImageURI();
 
         } else if (judge == 2) {
             cursor.moveToNext();
-            //URIが入っているカラムの箇所の取得
-            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-            //画像ファイルの固有番号を確認
-            Long id = cursor.getLong(fieldIndex);
-            //固有番号からURLの取得
-            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-
-            ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
-            imageVIew.setImageURI(imageUri);
+            setImageURI();
 
         } else if (judge == 3) {
             cursor.moveToPrevious();
-            //URIが入っているカラムの箇所の取得
-            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-            //画像ファイルの固有番号を確認
-            Long id = cursor.getLong(fieldIndex);
-            //固有番号からURLの取得
-            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+            setImageURI();
 
-            ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
-            imageVIew.setImageURI(imageUri);
+        } else if (judge == 4) {
+            String buttontxt4 = button4.getText().toString(); //入力文字の取得
+            Timer timer = new Timer(); //停止の場合にtimer.cancel();を使用したいため、ここでインスタンス生成
 
-        }else if(judge == 4) {
-            long period = 2; //タイマーの秒数をローカル変数として宣言
-            Timer.schedule(onClick(v.getId() == R.id.button2), period);
-
+            if(buttontxt4.equals("再生")) {
+                button4.setText("停止");  //ボタンに名「停止」に変更
+                long period = 2; //タイマーの秒数をローカル変数として宣言
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        cursor.moveToNext();
+                        setImageURI();
+                    }
+                }, null, period);
+            }else{
+                timer.cancel();
+            }
         }
+    }
 
+
+    public void setImageURI() {
+        //URIが入っているカラムの箇所の取得
+        int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+        //画像ファイルの固有番号を確認
+        Long id = cursor.getLong(fieldIndex);
+        //固有番号からURLの取得
+        Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+        ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
+        imageVIew.setImageURI(imageUri);
     }
 
     public void onClick(View v) {
